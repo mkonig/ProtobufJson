d69b642a3c74a9220607528d4a3849345fc02d5b
König, Mirko <mirko.konig@here.com>
2025-05-26 12:07:44 +0200

    Back to the roots.

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -7,7 +7,7 @@ pushd lib
 
 curl -L -o protobuf-cpp-3.21.2.tar.gz -C - https://github.com/protocolbuffers/protobuf/releases/download/v21.2/protobuf-cpp-3.21.2.tar.gz
 
-for x in *.tar.gz; do tar xf $x; done
+for x in *.tar.gz; do tar xf $x --no-same-owner; done
 
 pushd protobuf-3.21.2/
 ./configure --prefix=`pwd`/dist && make && make install
